fd0f499b67a2bf661bec8a444f44e0f887edb4b8
TamirDwek <mimiv007@gmail.com>
2025-03-09 12:13:36 +0200

    finish bemet

--- frontend/src/components/pages/card/Card.tsx
+++ frontend/src/components/pages/card/Card.tsx
@@ -11,16 +11,13 @@ export default function Card(props:CardProps):JSX.Element{
 
 const { groupId, meetingStartingTime, meetingDescription, meetingRoom} = props.meeting
 
-const {groupName} = props.meeting.developmentGroup || "Unknown";
 
-console.log("📌 Group Name:", groupName);
 
 return (
     <div className='Card'>
         <h4>Meeting Description:{meetingDescription}</h4>
         <p>Meeting Room: {meetingRoom}</p>
         <p>Meeting Time: {meetingStartingTime}</p>
-        <p>Group Name: {groupName}</p>
         <p>Group Id: {groupId}</p>
         
     </div>
